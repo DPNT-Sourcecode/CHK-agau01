9d3090f574c2e23e67a2947cd17f1578757a310e
sbx_user1051 <sbx_user1051@169.254.32.101>
2023-11-29 19:25:02 +0000

    Wed Nov 29 19:25:02 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -89,8 +89,68 @@ public class CheckoutSolution {
         return sum;
     }
 
+    public Integer checkout3(String skus) {
+        int numA = 0;
+        int numB = 0;
+        int numC = 0;
+        int numD = 0;
+        int numE = 0;
+        int numF = 0;
+
+        for (int i = 0; i < skus.length(); i++) {
+            char sku = skus.charAt(i);
+            if (sku == 'A') {
+                numA++;
+            } else if (sku == 'B') {
+                numB++;
+            } else if (sku == 'C') {
+                numC++;
+            } else if (sku == 'D') {
+                numD++;
+            } else if (sku == 'E') {
+                numE++;
+            } else if (sku == 'F') {
+                numF++;
+            } else {
+                return -1;
+            }
+        }
+
+        Integer sum = 0;
+
+        // SumA
+        sum += 200 * (numA / 5);
+        numA %= 5;
+        sum += 130 * (numA / 3);
+        sum += (numA % 3) * 50;
+
+        // SumE
+        sum += 40 * numE;
+        numB -= numE / 2;
+        if (numB < 0) {
+            numB = 0;
+        }
+
+        // SumB
+        sum += 45 * (numB / 2);
+        sum += (numB % 2) * 30;
+
+        // SumC
+        sum += 20 * numC;
+
+        // SumD
+        sum += 15 * numD;
+        
+        // SumF
+        sum += 20 * (numF / 3);
+        sum += 10 * (numF % 3);
+
+        return sum;
+    }
+
     public Integer checkout(String skus) {
-        return checkout2(skus);
+        return checkout3(skus);
     }
 }
+
 
